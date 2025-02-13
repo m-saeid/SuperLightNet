10df25c5fe5cb86e5b8c472814bd805ea4e51914
Mohammad Saeid <89332212+m-saeid@users.noreply.github.com>
2025-02-13 22:39:04 +0330

    Delete classification_ModelNet40_new1 directory

--- classification_ModelNet40_new1/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import absolute_import
-
-#from .SLNet import SLNet0, SLNet20, SLNet40, SLNet50, SLNet60, SLNet80, SLNet100
-from .models import SLNet_edge0_fps, SLNet_edge25_k3_fps, SLNet_edge25_k5_fps, SLNet_edge25_k25_fps, SLNet_edge25_k50_fps, SLNet_edge25_k75_fps, SLNet_edge25_k100_fps, SLNet_edge50_k3_fps, SLNet_edge50_k5_fps, SLNet_edge50_k25_fps, SLNet_edge50_k50_fps, SLNet_edge50_k75_fps, SLNet_edge50_k100_fps, SLNet_edge75_k3_fps, SLNet_edge75_k5_fps, SLNet_edge75_k25_fps, SLNet_edge75_k50_fps, SLNet_edge75_k75_fps, SLNet_edge75_k100_fps, SLNet_edge100_k3_fps, SLNet_edge100_k5_fps, SLNet_edge100_k25_fps, SLNet_edge100_k50_fps, SLNet_edge100_k75_fps, SLNet_edge100_k100_fps 
-
--- classification_ModelNet40_new1/run_test.sh
+++
@@ -1,2 +0,0 @@
-
-python test.py --checkpoint SLNet_edge0_fps-20250205061809-5539 --batch_size 16
